11066ded87b17c7ae6c93cf172d88df845beab2c
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-21 21:08:42 +0100

    Implement routing in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/home/Home/Home';
+import Likes from './components/likes/Likes/Likes';
+import Navbar from './components/main-layout/Navbar/Navbar';
+import NotFound from './components/NotFound/NotFound';
 
 const App = () => {
     return (
         <main>
-            App
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={ Home } />
+                    <Route path="/likes" component={ Likes } />
+                    <Route component={ NotFound } />
+                </Switch>
+            </Router>
         </main>
     );
 };
