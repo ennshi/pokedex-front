c14c80fa6860de0e5da4d8d05d68bb8e5bcfe5e0
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-22 19:41:55 +0100

    Using debouncer change search state on input change or on Enter

--- src/components/home/SearchBar/SearchBar.js
+++ src/components/home/SearchBar/SearchBar.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import SearchField from '../../common/SearchField/SearchField';
+import {debouncer, cancelDebouncer} from '../../../helpers/debouncer';
 
-const SearchBar = () => {
+const SearchBar = ({setSearch}) => {
+    const searchOnChange = (search) => {
+        debouncer(1000, () => setSearch(search));
+    };
+    const searchOnEnter = (key, search) => {
+        if(key === 'Enter') {
+            cancelDebouncer();
+            setSearch(search);
+        }
+    };
     return (
         <section>
-            <SearchField placeholder="Search ..." />
-            <SearchField placeholder="Number" />
-            <SearchField placeholder="Type" />
+            <SearchField
+                placeholder="Search ..."
+                handleChange={searchOnChange}
+                handleKeyDown={searchOnEnter}
+                name="name"
+            />
+            <SearchField
+                placeholder="Number"
+                handleChange={searchOnChange}
+                handleKeyDown={searchOnEnter}
+                name="id"
+            />
+            <SearchField
+                placeholder="Type"
+                handleChange={searchOnChange}
+                handleKeyDown={searchOnEnter}
+                name="type"
+            />
         </section>
     );
 };
