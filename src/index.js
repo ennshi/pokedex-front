166d07ff2463ee93d9a4bf08f53eec5c8120cc6e
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-22 23:24:36 +0100

    ModalContextProvider implemented

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ModalContextProvider} from './contexts/ModalContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ModalContextProvider>
+          <App />
+      </ModalContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
