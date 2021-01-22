e81e97324c3326304c65458bf15fc18c32ae988f
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-22 12:45:07 +0100

    Implement InfiniteScrollList in Home

--- src/components/home/Home/Home.js
+++ src/components/home/Home/Home.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PokemonList from '../../common/PokemonList/PokemonList';
 import SearchBar from '../SearchBar/SearchBar';
+import InfiniteScrollList from '../../common/InfiniteScrollList';
+import {LIMIT_POKEMONS, POKEMONS_URL} from '../../constants/FetchData';
 
 const Home = () => {
+    const [pokemons, setPokemons] = useState([]);
+    const [errors, setErrors] = useState(null);
     return (
         <section>
             <SearchBar />
-            <PokemonList />
+            {(!errors &&
+                <InfiniteScrollList
+                    url={POKEMONS_URL}
+                    setItems={setPokemons}
+                    setErrors={setErrors}
+                    items={pokemons}
+                    limitItems={LIMIT_POKEMONS}
+                >
+                    <PokemonList pokemons={pokemons}/>
+                </InfiniteScrollList>
+            )}
+            {(errors && <h5>Some error</h5>)}
         </section>
     );
 };
