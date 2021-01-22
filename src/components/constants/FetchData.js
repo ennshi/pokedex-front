783ef94c127911bccc0506a771b870d0c7ae815c
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-23 00:12:36 +0100

    Add pokemon details url

--- src/components/constants/FetchData.js
+++ src/components/constants/FetchData.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'http://localhost:8080';
 
 export const POKEMONS_URL = `${BASE_URL}/pokemons`;
+export const POKEMON_URL = `${BASE_URL}/pokemon`;
 export const LIKES_URL = `${BASE_URL}/liked-pokemons`;
 
 export const LIMIT_POKEMONS = 6;
