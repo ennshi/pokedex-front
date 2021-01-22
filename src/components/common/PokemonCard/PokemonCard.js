e89f770cb3562a2963468d39160e505ce1da22e2
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-23 00:13:15 +0100

    Apply setPokemonDetailsId in PokemonCard

--- src/components/common/PokemonCard/PokemonCard.js
+++ src/components/common/PokemonCard/PokemonCard.js
@@ -1,10 +1,16 @@
 import React, {useContext} from 'react';
 import {ModalContext} from '../../../contexts/ModalContext';
+import {PokemonDetailsContext} from '../../../contexts/PokemonDetailsContext';
 
 const PokemonCard = ({ pokemon }) => {
     const { toggleShowModal } = useContext(ModalContext);
+    const { setPokemonDetailsId } = useContext(PokemonDetailsContext);
+    const handleClick = () => {
+        setPokemonDetailsId(pokemon.id);
+        toggleShowModal();
+    };
     return (
-        <article onClick={toggleShowModal}>
+        <article onClick={ handleClick }>
             { pokemon.name }
         </article>
     );
