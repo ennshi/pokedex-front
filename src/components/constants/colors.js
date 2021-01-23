718617150eff8ef20b68384124346cc9cb2586b1
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-23 23:12:29 +0100

    Create color constants

---
+++ src/components/constants/colors.js
@@ -0,0 +1,20 @@
+export const TYPES_STYLING = {
+    'Normal': '#cacaca',
+    'Electric': '#ffe175',
+    'Fire': '#ff8a8a',
+    'Water': '#88d1fb',
+    'Ice': '#c6eaff',
+    'Poison': '#bf8cd1',
+    'Grass': '#b4fe7b',
+    'Fighting': '#ffb169',
+    'Flying': '#5f9fff',
+    'Steel': '#e4e4e4',
+    'Dragon': '#c699ff',
+    'Psychic': '#ffb7fc',
+    'Ground': '#ca9f5e',
+    'Rock': '#898373',
+    'Bug': '#d1e16f',
+    'Ghost': '#805594',
+    'Dark': '#444242',
+    'Fairy': '#fc83ff'
+};
