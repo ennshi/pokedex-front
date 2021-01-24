a139eaee602c9055c0cd7a7d0e7e0bcbb982f543
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-24 19:15:20 +0100

    Liked pokemons ids constant created

--- src/components/constants/FetchData.js
+++ src/components/constants/FetchData.js
@@ -3,5 +3,6 @@ const BASE_URL = 'http://localhost:8080';
 export const POKEMONS_URL = `${BASE_URL}/pokemons`;
 export const POKEMON_URL = `${BASE_URL}/pokemon`;
 export const LIKES_URL = `${BASE_URL}/liked-pokemons`;
+export const LIKED_POKEMON_IDS = `${BASE_URL}/liked-pokemons-ids`;
 
 export const LIMIT_POKEMONS = 6;
