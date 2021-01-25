af8c8468627424488ac9ac7611b5f587929c38a5
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-25 01:12:56 +0100

    Create Modal styling

--- src/components/common/modal/Modal.js
+++ src/components/common/modal/Modal.js
@@ -1,12 +1,18 @@
 import React, {useContext} from 'react';
 import {ModalContext} from '../../../contexts/ModalContext';
+import './Modal.css';
 
 const Modal = ({ children }) => {
     const { toggleShowModal } = useContext(ModalContext);
     return (
-        <section>
-            <div>
-                <button onClick={toggleShowModal}>&times;</button>
+        <section className="modal__container">
+            <div className="modal__close-container">
+                <button
+                    onClick={toggleShowModal}
+                    className="btn-close"
+                >
+                    &times;
+                </button>
             </div>
             { children }
         </section>
