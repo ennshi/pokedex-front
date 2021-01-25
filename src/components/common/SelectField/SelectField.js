a6b0c4f674470b9df41ac9a7e104d6d1caa67341
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-25 22:36:46 +0100

    Create SelectField

---
+++ src/components/common/SelectField/SelectField.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const SelectField = ({ name, options, classNames, handleKeyDown, handleChange, placeholder }) => {
+    return (
+        <select
+            name={name}
+            className={classNames}
+            onKeyDown={handleKeyDown}
+            onChange={handleChange}
+        >
+            <option value="" selected disabled> { placeholder } </option>
+            {options.length && options.map((op, i) => {
+                return (
+                    <option key={i} value={op.value}>
+                        {op.label}
+                    </option>
+                );
+            })}
+        </select>
+    );
+};
+
+export default SelectField;
