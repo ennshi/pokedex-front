d762e75841ada994a507cbd464330d800d358998
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-25 15:01:15 +0100

    Set image on error loading

--- src/components/common/Image/Image.js
+++ src/components/common/Image/Image.js
@@ -1,19 +1,26 @@
 import React, {useState} from 'react';
 import Skeleton from 'react-loading-skeleton';
+import { GiBigEgg } from 'react-icons/gi';
 
 const Image = ({ src, alt, styling, width, height }) => {
     const [imgLoaded, setImgLoaded] = useState(false);
+    const [error, setError] = useState(false);
     const isLoaded = () => {
         setImgLoaded(true);
     };
+    const notLoaded = () => {
+        setError(true);
+    };
     return (
         <>
-            {!imgLoaded && <Skeleton width={width} height={height} />}
+            {!imgLoaded && !error && <Skeleton width={width} height={height} />}
+            {error && <GiBigEgg className={styling} style={{color: '#c4c4c4'}} aria-label="egg image"/>}
             <img
                 src={src}
                 onLoad={isLoaded}
+                onError={notLoaded}
                 alt={alt}
-                className={imgLoaded ? styling : `${styling} hidden`}
+                className={(imgLoaded && !error) ? styling : `hidden`}
             />
         </>
     )
